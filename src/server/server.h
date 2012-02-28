28e7fef4d10f935e3e21bff6a9bc51a961e4540c
Martyanov Nikolay (netbook) <ohmspectator@gmail.com>
2012-02-28 12:51:56 +0400

    Reorganization: tabs size in server code decrimented

--- src/server/server.h
+++ src/server/server.h
@@ -7,40 +7,40 @@
 
 typedef enum s_id
 {
-	CONNECTION_ACCEPTED,
-	INIT_SEND,
-	AES_KEY_CREATED	
+  CONNECTION_ACCEPTED,
+  INIT_SEND,
+  AES_KEY_CREATED	
 } state_id;
 
 typedef enum m_type
 {
-	DH_GIVE_ME_INIT,
-	DH_TAKE_INIT,
-	DH_TAKE_MY_BASE
+  DH_GIVE_ME_INIT,
+  DH_TAKE_INIT,
+  DH_TAKE_MY_BASE
 } message_type;
 
 typedef struct c_state
 {
-	/* Client socket  */
-	int socket;
-	
-	/* Is length of next message known? 
-	   Known only if we've got all 2 bytes of length
-	*/
-	u_int8_t message_size_known;
+  /* Client socket  */
+  int socket;
 
-	/* Length of the next message */
-	u_int16_t message_size;
-	
-	/* Curret state identificator */
-	state_id current_state;
+  /* Is length of next message known? 
+     Known only if we've got all 2 bytes of length
+   */
+  u_int8_t message_size_known;
 
-	/* Wait for messge of the following type */
-	message_type next_message_type;
+  /* Length of the next message */
+  u_int16_t message_size;
 
-	/* Message buffer */
-	char message_buffer[BUFFER_SIZE];
+  /* Curret state identificator */
+  state_id current_state;
+
+  /* Wait for messge of the following type */
+  message_type next_message_type;
+
+  /* Message buffer */
+  char message_buffer[BUFFER_SIZE];
 
 } connection_state;
 
- 
+
