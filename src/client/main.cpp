96e690cd9e2898861eff4f1029db1ca8e6b98e3d
Martyanov Nikolay (PC-Linux) <ohmspectator@gmail.com>
2012-03-27 17:24:05 +0400

    Client: forever ask for a new msg

--- src/client/main.cpp
+++ src/client/main.cpp
@@ -13,7 +13,10 @@ int main()
   const char* server_address = console.get_server_addr();
   const char* server_port = console.get_server_port();
   client.connect_to_server( server_address, server_port );
-  string message = console.ask_for_message();
-  client.send_message((unsigned char*)(message.c_str()), message.length());
+  while(1)
+  {
+    string message = console.ask_for_message();
+    client.send_message((unsigned char*)(message.c_str()), message.length());
+  }
   return 0;
 }
